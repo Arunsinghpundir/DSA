e822dbc92959b2b6822a3f04bafe8fa3fddcd87a
arun singh <arunsinghpundir.325@gmail.com>
2022-08-23 15:46:56 +0530

    LinkedList

--- src/DSA2/LinkedList/LinkedList.java
+++ src/DSA2/LinkedList/LinkedList.java
@@ -72,4 +72,30 @@ public class LinkedList {
         }
         return prev;
     }
+    public Node  removeNthFromEnd(int data){
+        if(head==null){
+            return null;
+        }
+        int size = 0;
+        Node curr = head;
+        while(curr != null){
+            curr = curr.next;
+            size++;
+        }
+
+        if(data==size){
+            return head.next;
+        }
+
+        int indexToRemove = size-data;
+        int i = 1;
+        Node prev = head;
+        while(i<indexToRemove){
+            prev = prev.next;
+            i++;
+        }
+
+        prev.next = prev.next.next;
+        return head;
+    }
 }
--- src/DSA2/LinkedList/Main.java
+++ src/DSA2/LinkedList/Main.java
@@ -7,35 +7,13 @@ public class Main {
         ll.insert(1);
         ll.insert(2);
         ll.insert(3);
-
         ll.insert(4);
         ll.insert(5);
         ll.insert(6);
-
-        System.out.println(" ");
-
-        System.out.print("Middle element is :" + ll.middle().data + " \n" );
-
-        ll.printFromMiddle();
-        System.out.println(" ");
-
-
-        ll.head = ll.ReverseList(ll.head);
+        ll.removeNthFromEnd(3);
         ll.print();
-        System.out.println();
-        ll.printFromMiddle();
-        System.out.println();
-
-
-        ll.head = ll.reverseList_Recursive(ll.head);
-
-        System.out.println();
-
-        ll.insert(5);
-        ll.insert(1000);
-        ll.printFromMiddle();
-        ll.insert(500);
-
+        ll.removeNthFromEnd(4);
+        ll.print();
 
     }
 }
